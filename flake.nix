569abc0ee227db8a515e8600dfda617657df9933
stellessia <homicide@disroot.org>
2024-12-19 15:56:36 +0200

    Updated the vendorHash

--- flake.nix
+++ flake.nix
@@ -44,7 +44,7 @@
             inherit version;
             src = ./caddy-src;
             runVend = true;
-            vendorHash = "sha256-c9A0LabGN8gIq2pL/WP8wst3y0+8f72K015QZrX9yq4=";
+            vendorHash = "sha256-6tXkhfHbD84P7Awb38Wgq+otO3cTKhCfBtCjwhtrJK4=";
             # vendorHash = pkgs.lib.fakeHash;
 
             meta = {
